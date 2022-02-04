ce051bacc49f47b96a6187183d5515a0a0f7bd8a
Vysagh <vthambi@korewireless.com>
2022-02-04 14:37:25 +0530

    Generated random array

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { useState } from "react";
 import Dice from "./components/Dice";
+import { nanoid } from 'nanoid'
 
 function App() {
+  const [diceArr,setDiceArr] = useState(generateRandomArr);
+
+  function generateRandomArr() {
+    const newArr = [];
+    for (let index = 0; index < 10; index++) {
+      let randomValue = {
+        id: nanoid(),
+        digit: Math.ceil(Math.random() * 6),
+        isHeld: false
+      }
+      newArr.push(randomValue)
+    }
+    return newArr;
+  }
+  console.log(diceArr)
+
   return (
     <div className="main mt-5 d-flex flex-column justify-content-center">
       <h2 className="text-center pt-3">Dice Game</h2>
